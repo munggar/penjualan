118f5a18e1e8de498af7fa7d22925156a92b4bdf
munggar <igarakun2@gmail.com>
2025-07-31 09:23:57 +0700

    Update views

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Dashboard - Toko Jahit</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ asset('app.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
 </head>
 
 <body>
